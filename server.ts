88774b2b8712bac9592f913674a509df8aaa1b6d
Sam Sumiya <60025491+SamSumiya@users.noreply.github.com>
2025-07-01 15:04:52 -0700

    added getting data from dbAsync()

--- server.ts
+++ server.ts
@@ -1,13 +1,21 @@
 import http, { IncomingMessage, ServerResponse } from 'node:http'
-
+import { dbAsync } from './db'
 const PORT = Number(process.env.PORT) || 8000 
+
 
 const server = http.createServer(
     (request:IncomingMessage, response:ServerResponse) => {
         console.log(request.method)
         response.statusCode = 200
-        response.setHeader("Content-Type", "text/plain")
-        response.end('Hi TypeScript!\n')
+        response.setHeader("Content-Type", "application/json")
+        dbAsync()
+            .then(data => response.end(JSON.stringify(data)))
+            .catch(err => {
+                console.error(err)
+                response.statusCode = 500
+                response.end('Internal Server Error')  
+            })
+        // response.end('Hi TypeScript v2!\n')
 })
 
 server.listen(PORT, () => {
